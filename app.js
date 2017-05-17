97f29ef47b3da85900e1e31c44ddecd05ac9c4de
Sonja Duric <duric.sonja@gmail.com>
2017-05-17 12:59:36 -0400

    callback functions are strange

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const appState ={
+const appState = {
     resultThumbnails: [],
 }
 const urlYoutube ="https://www.googleapis.com/youtube/v3/search";
@@ -8,5 +8,63 @@ function getJsonElement(searchTerm,callback ){
         key: 'AIzaSyBJaTa4o4ogOUb44z5YIoA6h692_IXy-js',
         q: searchTerm,
     }
-    $.getJson(urlYoutube,query,callback);
+    $.getJSON(urlYoutube,query,callback);
 }
+
+const search = "cats";
+
+function storeThumbs(appState, thumbs){
+	thumbs.forEach(thumb => appState.resultThumbnails.push(thumb));
+	
+}
+
+function getThumbs(response){
+	console.log("yo i did something");
+	const urls = response.items.map(el=>(el.snippet.thumbnails.medium.url));
+	console.log(appState.resultThumbnails);
+	return urls;
+}
+
+function renderThumbs(appState, element){
+	let thumbnails = appState.resultThumbnails;
+	console.log(appState.resultThumbnails);
+
+	let html = `<ul>`;
+
+	for(let i=0; i < thumbnails.length; i++){
+		console.log(thumbnails[i]);
+		html+=`<li><img src=${thumbnails[i]} alt="cat_${i}"></li>`;
+	}
+
+	// html+= (thumbnails.forEach(thumb =>
+	// 	`<li><img src=${thumb} alt="cat"></li>`)).join();
+
+	html += `</ul>`;
+	console.log(html);
+	element.html(html);
+	element.removeClass('hidden');
+}
+
+
+
+function addListeners(){
+
+	$('form').on('submit', function(event){
+		event.preventDefault();
+		const search = $('#search-youtube').val();
+		console.log(search);
+		getJsonElement(search, getThumbs);
+		console.log(appState.resultThumbnails);
+		renderThumbs(appState, $('.thumbnails'));
+	});
+
+}
+
+$(function () {
+
+addListeners();
+
+});
+
+//getJsonElement(search, storeThumbs);
+
